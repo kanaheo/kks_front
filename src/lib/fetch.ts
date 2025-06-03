d942f9d5a667040b3002792696faeb1ba1d87bcc
KyoungMin HEO <stalvede0421@gmail.com>
2025-06-03 16:37:59 +0900

    fetch api base env file change

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8080/api";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL! + "/api";
 
 export const fetchGet = async (url: string) => {
   const fullUrl = `${API_BASE}${url}`;
